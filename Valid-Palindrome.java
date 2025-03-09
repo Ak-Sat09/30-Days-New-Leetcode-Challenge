50a3fc8023ab18baa3fc83a729a7ba0277538ece
Anmol Mehla <166195625+Ak-Sat09@users.noreply.github.com>
2025-03-09 10:39:17 +0530

    [125] [Time Beats: 23.35%] [Memory Beats: 5.08%] - LeetPush

---
+++ Valid-Palindrome.java
@@ -0,0 +1,16 @@
+ public class Solution {
+    public boolean isPalindrome(String str) { 
+        char[] arr = str.replaceAll(\[^a-zA-Z0-9]\, \\).toLowerCase().toCharArray(); 
+        return pali(arr , 0 , arr.length-1);
+    }
+
+    private boolean pali(char[] arr, int left, int right) {
+        if (left >= right) {
+            return true;
+        }
+      if (arr[left] != arr[right]) {
+            return false;
+        } 
+      return pali(arr, left + 1, right - 1);
+    }
+}
