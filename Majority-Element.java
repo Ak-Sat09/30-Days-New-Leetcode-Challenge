ff7bbbbbacbabf4723ef1a45e367b4566544c042
Anmol Mehla <166195625+Ak-Sat09@users.noreply.github.com>
2025-03-12 11:14:52 +0530

    [169] [Time Beats: 99.88%] [Memory Beats: 59.41%] - LeetPush

---
+++ Majority-Element.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int majorityElement(int[] arr) {
+        int n = arr.length;
+        int can = arr[0];
+        int count = 0;
+        for(int i : arr){
+            if(count == 0){
+                can = i;
+            }
+             if(i == can){
+                count++;
+            }
+            else{
+                count--;
+            }
+        }
+        int freq = 0;
+        for(int i : arr){
+            if(i == can){
+                freq++;
+            }
+        } 
+        if(freq > n/2){
+            return can;
+        }   
+        return -1;
+        }
+}
