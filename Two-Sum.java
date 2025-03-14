930c09983d75b16d2c0da3ebf6d9ec5908158afc
Anmol Mehla <166195625+Ak-Sat09@users.noreply.github.com>
2025-03-14 12:47:58 +0530

    [1] [Time Beats: 98.85%] [Memory Beats: 5.10%] - LeetPush

--- Two-Sum.java
+++ Two-Sum.java
@@ -2,18 +2,14 @@ import java.util.*;
 
 class Solution {
     public int[] twoSum(int[] arr, int target) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        
-        for (int i = 0; i < arr.length; i++) {  // Corrected loop
-            int comp = target - arr[i]; // Calculate complement
-            
-            if (map.containsKey(comp)) {  // Check if complement exists
-                return new int[]{i, map.get(comp)}; 
+        HashMap<Integer ,Integer> map = new HashMap<>();
+        for(int i=0;i<arr.length; i++){
+            int comp = target-arr[i];
+            if(map.containsKey(comp)){
+                return new int[] {i , map.get(comp)};
             }
-            
-            map.put(arr[i], i);  // Store the element and its index
+            map.put(arr[i],i);
         }
-        
-        return new int[]{};  // Return an empty array if no pair is found
+        return new int[]{};
     }
 }
