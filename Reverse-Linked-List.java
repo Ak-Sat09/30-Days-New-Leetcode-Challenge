8d82af4a96d7e491b3ff936b41c74a4e066d58d6
Anmol Mehla <166195625+Ak-Sat09@users.noreply.github.com>
2025-03-14 12:37:11 +0530

    [206] [Time Beats: 5.20%] [Memory Beats: 39.14%] - LeetPush

--- Reverse-Linked-List.java
+++ Reverse-Linked-List.java
@@ -1,13 +1,20 @@
 class Solution {
     public ListNode reverseList(ListNode head) {
+        if(head == null)return null;
+
+    Stack<ListNode> stack = new Stack<>(); 
     ListNode curr = head;
-    ListNode prev = null;
     while(curr != null){
-        ListNode next = curr.next;
-        curr.next = prev;
-        prev = curr;
-        curr = next;
+        stack.push(curr);
+        curr = curr.next;
     }
-    return prev;
+    ListNode newhead = stack.pop();
+    curr = newhead;
+    while(!stack.isEmpty()){
+        curr.next = stack.pop();
+        curr = curr.next;
+    }
+    curr.next = null;
+    return newhead;
     }
 }
