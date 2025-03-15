c5e1c09d28c1fa5385eaba5dc87da69c20c42de8
Anmol Mehla <166195625+Ak-Sat09@users.noreply.github.com>
2025-03-15 18:15:27 +0530

    [176] [Time Beats: 5.00%] - LeetPush

--- Second-Highest-Salary.sql
+++ Second-Highest-Salary.sql
@@ -1,6 +1,3 @@
- SELECT 
-    (SELECT DISTINCT salary 
-     FROM Employee 
-     ORDER BY salary DESC 
-     LIMIT 1 OFFSET 1) 
-    AS SecondHighestSalary;
+SELECT MAX(Salary) AS SecondHighestSalary  
+FROM Employee  
+WHERE Salary < (SELECT MAX(Salary) FROM Employee);
