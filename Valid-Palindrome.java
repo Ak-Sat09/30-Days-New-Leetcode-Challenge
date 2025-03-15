9435a72c7201312cc26613b782dfe15a3e77081d
Anmol Mehla <166195625+Ak-Sat09@users.noreply.github.com>
2025-03-15 11:13:39 +0530

    [125] [Time Beats: 34.00%] [Memory Beats: 5.10%] - LeetPush

--- Valid-Palindrome.java
+++ Valid-Palindrome.java
@@ -5,12 +5,8 @@
     }
 
     private boolean pali(char[] arr, int left, int right) {
-        if (left >= right) {
-            return true;
-        }
-      if (arr[left] != arr[right]) {
-            return false;
-        } 
-      return pali(arr, left + 1, right - 1);
+      if(left > right)return true;
+      if(arr[left] != arr[right])return false;
+      return pali(arr,left+1,right-1);
     }
 }
