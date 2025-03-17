bf574988078c049128bdd0adbe1e0bec24d5afb6
Anmol Mehla <166195625+Ak-Sat09@users.noreply.github.com>
2025-03-17 15:26:29 +0530

    [75] [Time Beats: 100.00%] [Memory Beats: 45.95%] - LeetPush

--- Sort-Colors.java
+++ Sort-Colors.java
@@ -1,31 +1,29 @@
-class Solution {
+ class Solution {
     public void sortColors(int[] arr) {
-        int count0 = 0;
-        int count1 = 0;
-        int count2 = 0;
+       int count0 = 0;
+       int count1 = 0;
+       int count2 = 0;
 
-        for(int i=0; i<arr.length; i++){
-            if(arr[i] == 0){
-                count0++;
-            }
-            else if(arr[i] == 1){
-                count1++;
-            }
-            else{
-                count2++;
-            }
+       for(int i=0; i<arr.length; i++){
+        if(arr[i] == 0){
+            count0++;
         }
-
-        for(int i=0; i<arr.length; i++){
-            if(i<count0){
-                arr[i] = 0;
-            }
-            else if(i < count0+count1){
-                arr[i] = 1;
-            }
-            else{
-                arr[i] = 2;
-            }
+        else if(arr[i] == 1){
+            count1++;
         }
+        else{
+            count2++;
+        }
+       }
+       for(int i=0; i<arr.length; i++){
+        if(i<count0){
+            arr[i] = 0;
+        }else if(i < count0+count1){
+            arr[i] = 1;
+        }
+        else{
+            arr[i] = 2;
+        }
+       }
     }
 }
