738e8fa40b2363a30bb5c4030a2ddf8385021b9a
Anmol Mehla <166195625+Ak-Sat09@users.noreply.github.com>
2025-03-17 14:33:18 +0530

    [162] [Time Beats: 100.00%] [Memory Beats: 72.39%] - LeetPush

--- Find-Peak-Element.java
+++ Find-Peak-Element.java
@@ -3,10 +3,11 @@ class Solution {
      int left = 0;
      int right = arr.length-1;
      while(left < right){
-        int mid = left+(right-left)/2;
+        int mid = left + (right - left)/2;
         if(arr[mid] < arr[mid+1]){
             left = mid+1;
-        }else{
+        }
+        else{
             right = mid;
         }
      }
