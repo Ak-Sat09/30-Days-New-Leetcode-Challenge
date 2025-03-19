04cb1c24dacb5becfe9a182e18d615214ab2aeb6
Anmol Mehla <166195625+Ak-Sat09@users.noreply.github.com>
2025-03-19 10:50:50 +0530

    [128] [Time Beats: 19.40%] [Memory Beats: 26.28%] - LeetPush

--- Longest-Consecutive-Sequence.java
+++ Longest-Consecutive-Sequence.java
@@ -1,21 +1,24 @@
 class Solution {
     public int longestConsecutive(int[] arr) {
-        if (arr.length == 0) return 0;
-        Arrays.sort(arr);
-        int count = 1;
-        int max = 1;
-        for(int j=1; j<arr.length; j++){
-            if(arr[j] == arr[j-1]){
-                continue;
-            }
-            else if(arr[j] == arr[j-1]+1){
-                count++;
-            }
-            else{
-                count = 1;
-            }
-            max = Math.max(max, count);
+        HashSet<Integer> set = new HashSet<>();
+        for(int i : arr){
+            set.add(i);
         }
-        return max;
+        int maxLen = 0;
+        for(int i : set){
+            if(!set.contains(i-1)){
+                int currNum = i;
+                int count = 1;
+
+                while(set.contains(currNum+1)){
+                    currNum++;
+                    count++;
+                }
+            
+            maxLen = Math.max(maxLen , count);
+            }
+        }
+        return maxLen;
     }
 }
+ 
