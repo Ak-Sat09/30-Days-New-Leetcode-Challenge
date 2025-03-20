13e994f92531b5a5c9f1d9e307c94ccbc0ec9c5a
Anmol Mehla <166195625+Ak-Sat09@users.noreply.github.com>
2025-03-20 10:11:31 +0530

    [387] [Time Beats: 47.84%] [Memory Beats: 65.29%] - LeetPush

---
+++ First-Unique-Character-in-a-String.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int firstUniqChar(String str) {
+        HashMap<Character , Integer> map = new HashMap<>();
+        for(char ch : str.toCharArray()){
+            map.put(ch , map.getOrDefault(ch,0)+1);
+        }
+          for (int i = 0; i < str.length(); i++) {
+            if (map.get(str.charAt(i)) == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
