7c1165effc673d1015daafc7c3b83ea2841dee8e
Anmol Mehla <166195625+Ak-Sat09@users.noreply.github.com>
2025-03-24 11:58:03 +0530

    [1] [Time Beats: 63.72%] [Memory Beats: 62.56%] - LeetPush

--- Two-Sum.java
+++ Two-Sum.java
@@ -1,12 +1,10 @@
-import java.util.*;
-
 class Solution {
     public int[] twoSum(int[] arr, int target) {
-      HashMap<Integer , Integer> map = new HashMap<>();
+      HashMap<Integer ,Integer> map = new HashMap<>();
       for(int i=0; i<arr.length; i++){
-        int comp = target-arr[i];
+        int comp = target - arr[i];
         if(map.containsKey(comp)){
-            return new int[] {map.get(comp) , i};
+            return new int[] {i,map.get(comp)};
         }
         map.put(arr[i],i);
       }
