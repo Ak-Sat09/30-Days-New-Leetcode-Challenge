cc4a30adaf04f1de7633ac2afb0cea179044234f
Anmol Mehla <166195625+Ak-Sat09@users.noreply.github.com>
2025-03-24 19:28:23 +0530

    [739] [Time Beats: 52.01%] [Memory Beats: 9.97%] - LeetPush

--- Daily-Temperatures.java
+++ Daily-Temperatures.java
@@ -1,16 +1,15 @@
  class Solution {
     public int[] dailyTemperatures(int[] arr) {
-        int n = arr.length;
-        int res[] = new int[n];
-
-        Stack<Integer> stack = new Stack<>();
-        for(int i=0; i<arr.length; i++){
-            while(!stack.isEmpty() && arr[i] > arr[stack.peek()]){
-                int last = stack.pop();
-                res[last] = i-last;
-            }
-            stack.push(i);
+      int n = arr.length;
+      int res[] = new int[n];
+      Stack<Integer> stack = new Stack<>();
+      for(int i=0; i<n; i++){
+        while(!stack.isEmpty() && arr[stack.peek()] < arr[i]){
+          int idx =  stack.pop();
+          res[idx] = i-idx;
         }
-        return res;
+        stack.push(i);
+      }
+      return res;
     }
 }
