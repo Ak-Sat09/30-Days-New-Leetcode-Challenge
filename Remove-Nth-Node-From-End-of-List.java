e61eba2d02f88c75d8d20c831f838d83f8e1b65d
Anmol Mehla <166195625+Ak-Sat09@users.noreply.github.com>
2025-03-27 10:55:40 +0530

    [19] [Time Beats: 100.00%] [Memory Beats: 7.70%] - LeetPush

--- Remove-Nth-Node-From-End-of-List.java
+++ Remove-Nth-Node-From-End-of-List.java
@@ -6,13 +6,10 @@
             len++;
             curr = curr.next;
         }
-
         int m = len-n;
         if(m==0)return head.next;
-
         curr = head;
-
-         for(int i=1; i<m; i++){
+        for(int i=0; i<m-1; i++){
             curr = curr.next;
         }
         curr.next = curr.next.next;
