30d543d87611ef8567f0f8ac7d6cc2f877be77fa
Anmol Mehla <166195625+Ak-Sat09@users.noreply.github.com>
2025-03-29 14:11:35 +0530

    [402] [Time Beats: 57.29%] [Memory Beats: 76.13%] - LeetPush

--- Remove-K-Digits.java
+++ Remove-K-Digits.java
@@ -1,34 +1,30 @@
-import java.util.Stack;
-
 class Solution {
     public String removeKdigits(String str, int k) {
         Stack<Character> stack = new Stack<>();
-        
-        for (char ch : str.toCharArray()) {
-            while (!stack.isEmpty() && k > 0 && stack.peek() > ch) {
+        for(char ch : str.toCharArray()){
+            while(!stack.isEmpty() && k>0 && stack.peek() > ch){
                 stack.pop();
                 k--;
             }
-            stack.push(ch); // Push the current digit
+            stack.push(ch);
         }
+            while(k>0 && !stack.isEmpty()){
+                stack.pop();
+                k--;
+            }
+            StringBuilder res = new StringBuilder();
+            for(char c : stack){
+                res.append(c);
+            }
 
-        // Remove remaining k elements from the end if needed
-        while (k > 0 && !stack.isEmpty()) {
-            stack.pop();
-            k--;
+            while(res.length() > 0 && res.charAt(0) =='0'){
+                res.deleteCharAt(0);
+            }
+        
+        if(res.length() == 0){
+            return \0\;
         }
-
-        // Build result string
-        StringBuilder res = new StringBuilder();
-        for (char c : stack) {
-            res.append(c);
-        }
-
-        // Remove leading zeros
-        while (res.length() > 0 && res.charAt(0) == '0') {
-            res.deleteCharAt(0);
-        }
-
-        return res.length() == 0 ? \0\ : res.toString();
+        
+        return res.toString();
     }
 }
