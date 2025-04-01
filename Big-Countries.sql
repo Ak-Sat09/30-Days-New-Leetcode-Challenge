520e6adca64f78b1506fb22acd2e15b18bb39335
Anmol Mehla <166195625+Ak-Sat09@users.noreply.github.com>
2025-04-01 17:01:13 +0530

    [595] [Time Beats: 74.54%] - LeetPush

--- Big-Countries.sql
+++ Big-Countries.sql
@@ -1,5 +1,2 @@
-select name , population , area
-from World 
-where population  >= 25000000 
-OR 
-area >= 3000000
+SELECT name , population , area FROM World
+WHERE area >= 3000000 OR population >= 25000000
