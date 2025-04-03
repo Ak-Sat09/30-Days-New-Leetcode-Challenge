0cf6dbff1d038ce35db90c816b1600ebabe6446f
Anmol Mehla <166195625+Ak-Sat09@users.noreply.github.com>
2025-04-03 20:07:21 +0530

    [287] [Time Beats: 89.61%] [Memory Beats: 16.86%] - LeetPush

--- Find-the-Duplicate-Number.java
+++ Find-the-Duplicate-Number.java
@@ -1,17 +1,18 @@
  class Solution {
     public int findDuplicate(int[] arr) { 
-       int slow = arr[0];
-       int fast = arr[0];
-       do{
+    int slow = arr[0];
+    int fast = arr[0];
+
+    do{
         slow = arr[slow];
         fast = arr[arr[fast]];
-       }while(slow != fast);
+    }while(slow != fast);
 
-       slow = arr[0];
-       while(slow != fast){
+    slow = arr[0];
+    while(slow != fast){
         slow = arr[slow];
         fast = arr[fast];
-       }
-       return slow;
+    }
+    return  slow;
     }
 }
