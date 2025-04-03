f6c22bf8c2dd9b5ea906cfa23459d8fbaef8e48e
Anmol Mehla <166195625+Ak-Sat09@users.noreply.github.com>
2025-04-03 20:19:34 +0530

    [11] [Time Beats: 74.84%] [Memory Beats: 88.31%] - LeetPush

--- Container-With-Most-Water.java
+++ Container-With-Most-Water.java
@@ -1,20 +1,21 @@
  class Solution {
     public int maxArea(int[] arr) {
     int n = arr.length;
-    int i = 0;
-    int j= n-1;
-    int maxArea = 0;
-    while(i < j){
-        int hgt = Math.min(arr[i],arr[j]);
-        int wdt = j-i;
-        int area= wdt*hgt;
-        maxArea = Math.max(maxArea,area);
-        if(arr[i]<arr[j]){
-            i++;
+    int left = 0;
+    int right = n-1;
+    int maxLen = 0;
+    while(left<=right){
+        int hgt = Math.min(arr[left],arr[right]);
+        int wdt = right-left;
+        int area = hgt*wdt;
+
+        maxLen = Math.max(maxLen,area);
+        if(arr[left] < arr[right]){
+            left++;
         }else{
-            j--;
+            right--;
         }
     }
-    return maxArea;
+    return maxLen;
     }
 }
