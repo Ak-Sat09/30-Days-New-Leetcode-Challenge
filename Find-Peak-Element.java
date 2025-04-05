52adc627b78c90489ccbd2ab7c4a2c90305b1fac
Anmol Mehla <166195625+Ak-Sat09@users.noreply.github.com>
2025-04-05 20:39:19 +0530

    [162] [Time Beats: 100.00%] [Memory Beats: 42.77%] - LeetPush

--- Find-Peak-Element.java
+++ Find-Peak-Element.java
@@ -1,15 +1,19 @@
-class Solution {
-    public int findPeakElement(int[] arr){
-       int left = 0;
-       int right = arr.length-1;
-       while(left < right){
-        int mid = left+(right-left)/2;
-        if(arr[mid] < arr[mid+1]){
-            left = mid+1;
-        }else{
-            right = mid;
+ class Solution {
+    public int findPeakElement(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] > nums[mid + 1]) {
+                // Peak is on left side or at mid
+                right = mid;
+            } else {
+                // Peak is on right side
+                left = mid + 1;
+            }
         }
-       }
-       return left;
+        return left; // or return nums[left] if you want the value
     }
 }
