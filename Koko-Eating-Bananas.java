de9e0aafb95a4d36b510e17602425d3ba826da74
Anmol Mehla <166195625+Ak-Sat09@users.noreply.github.com>
2025-04-06 18:45:32 +0530

    [875] [Time Beats: 64.38%] [Memory Beats: 46.72%] - LeetPush

---
+++ Koko-Eating-Bananas.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int minEatingSpeed(int[] arr, int h) {
+        
+        int low  = 1 , high = Integer.MAX_VALUE;
+        for(int i : arr){
+            high = Math.max(high , i);
+        }
+        int res = high;
+        while(low <= high){
+            int mid = low +(high-low)/2;
+            int hrs = 0;
+            for(int i : arr){
+                if(i % mid == 0){
+                    hrs += i/mid;
+                }
+                else{
+                    hrs += (i/mid)+1;
+                }
+            }
+            if(hrs <= h){
+                res = mid;
+                high = mid-1;
+            }else{
+                low = mid+1;
+            }
+        }
+        return res;
+    }
+}
