9927199b05ac2e4f9a86ee55693a51a2d1d9afbf
Anmol Mehla <166195625+Ak-Sat09@users.noreply.github.com>
2025-04-12 18:04:25 +0530

    [238] [Time Beats: 88.04%] [Memory Beats: 62.69%] - LeetPush

--- Product-of-Array-Except-Self.java
+++ Product-of-Array-Except-Self.java
@@ -1,16 +1,21 @@
  class Solution {
     public int[] productExceptSelf(int[] arr) {
-      int n = arr.length;
-      int ans[] = new int[n];
-      ans[0] = 1;
-      for(int i=1; i<arr.length; i++){
-        ans[i] = ans[i-1]*arr[i-1];
-      }
-      int suffix = 1;
-      for(int i=n-1; i>=0; i--){
-        ans[i] = ans[i]*suffix;
-        suffix = suffix*arr[i];
-      }
-      return ans;
+        int n = arr.length;
+        int[] res = new int[n];
+        res[0] = 1;
+
+      for (int i = 1; i < n; i++) {
+            res[i] = arr[i - 1] * res[i - 1];
+            
+            }
+
+            int suffix = 1;
+
+            for(int i=n-1; i>=0; i--){
+                res[i] = suffix*res[i];
+                suffix = suffix*arr[i];
+            }
+
+            return res;
     }
 }
